6b11bc53f8d3ff509911ed244da5845739b4887d
Ilia Shadilow <sveta.shadilowa@yandex.ru>
2026-09-26 23:28:21 +0700

    готовый сайдбар после фикса ошибок линтера

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,6 @@
-import { defaultArticleState } from '@/constants/articleProps.ts';
+import { defaultArticleState, type ArticleStateType } from '@/constants/articleProps.ts';
 import { clsx } from 'clsx';
+import { useState } from 'react';
 
 import { ArticleParamsForm } from '@components/article-params-form';
 
@@ -10,20 +11,28 @@ import type { CSSProperties } from 'react';
 import styles from './app.module.scss';
 
 export const App = (): React.JSX.Element => {
+  const [articleState, setArticleState] =
+    useState<ArticleStateType>(defaultArticleState); // стейт для примененных настроек
+
+  const handleApply = (newState: ArticleStateType): void => {
+    // фун-я, которую передаем в форму
+    setArticleState(newState);
+  };
+
   return (
     <main
       className={clsx(styles.main)}
       style={
         {
-          '--font-family': defaultArticleState.fontFamilyOption.value,
-          '--font-size': defaultArticleState.fontSizeOption.value,
-          '--font-color': defaultArticleState.fontColor.value,
-          '--container-width': defaultArticleState.contentWidth.value,
-          '--bg-color': defaultArticleState.backgroundColor.value,
+          '--font-family': articleState.fontFamilyOption.value,
+          '--font-size': articleState.fontSizeOption.value,
+          '--font-color': articleState.fontColor.value,
+          '--container-width': articleState.contentWidth.value,
+          '--bg-color': articleState.backgroundColor.value,
         } as CSSProperties
       }
     >
-      <ArticleParamsForm />
+      <ArticleParamsForm onApply={handleApply} /> {/* передаем функцию в форму */}
       <Article />
     </main>
   );
